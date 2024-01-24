392089aede096d6c47dc7899015e5a47f647d83e
ddrongy <ddrongy@gmail.com>
2024-01-24 17:51:00 +0900

    fix: auth가 필요한 get/post 추가
    - get, post하기 전에 autenticated가 필요한 경우라면 별도의 로직에서 처리
      - 존재하는 cookie인지 확인함
      - request에서 header에 Cookie("sid")가 오는 경우에 진행

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,22 +1,46 @@
 package controller;
 
+import db.Session;
 import http.request.HttpMethod;
 import http.request.Request;
 import http.response.Response;
 
 public abstract class Controller {
+
     public void service(Request request, Response response) {
         if (request.getMethod() == HttpMethod.GET) {
-            doGet(request, response);
+            if (isAuthenticated(request)) {
+                doGetAuthenticated(request, response);
+            } else {
+                doGet(request, response);
+            }
+        } else if (request.getMethod() == HttpMethod.POST) {
+            if (isAuthenticated(request)) {
+                doPostAuthenticated(request, response);
+            } else {
+                doPost(request, response);
+            }
         }
-        if (request.getMethod() == HttpMethod.POST) {
-            doPost(request, response);
-        }
+    }
+
+    private boolean isAuthenticated(Request request) {
+        String sessionId = getSessionIdFromHeader(request);
+        return sessionId != null && Session.isValidSession(sessionId);
+    }
+
+    private String getSessionIdFromHeader(Request request) {
+        return request.getHeader().getCookie();
     }
 
     public void doGet(Request request, Response response) {
     }
 
     public void doPost(Request request, Response response) {
+    }
+
+    public void doGetAuthenticated(Request request, Response response) {
+    }
+
+    public void doPostAuthenticated(Request request, Response response) {
     }
 }
